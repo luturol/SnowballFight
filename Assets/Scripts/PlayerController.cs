a079d565128abc4f8ac5948705f7e48179e86f62
Rafael Ahrons <rafael.ahrons@gmail.com>
2019-08-31 20:53:07 -0300

    Add controls to player and Fix Friction Bug on Platforms

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+    public KeyCode throwBall;
+
+    private Rigidbody2D rigidbody2D;
+
+    //Controlar se o player está no chão ou não
+    public Transform groundCheckPoint;
+    public bool isGrounded;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround; //Default layer = Tudo que está nela, ou seja, by default são os componentes criados no unity
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vai desenhar um circulo envolta do ponto e ver se ele está no chão ou não e retornar um bool
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+
+        if(Input.GetKey(left))
+        {
+            rigidbody2D.velocity = new Vector2(-moveSpeed, rigidbody2D.velocity.y);
+        }
+        else if (Input.GetKey(right))
+        {
+            rigidbody2D.velocity = new Vector2(moveSpeed, rigidbody2D.velocity.y);
+        }
+        else
+        {
+            rigidbody2D.velocity = new Vector2(0, rigidbody2D.velocity.y);
+        }
+
+        //Pode pular somente se estiver no chão
+        //Faz com que o player só consiga pular uma vez e não infinitos pulos
+        if(Input.GetKeyDown(jump) && isGrounded)
+        {
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpForce);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,17 @@ It's a 2D game created with Unity 2018.3.7f1 using the tutorials from [GamePlusJ
 ## [Video 1](https://www.youtube.com/watch?v=p23J5-1OTAM)
 
 In this video you will learn how to split sprites and how to add box colliders into your platform objects and how to change size of it.
+
+## [Video 2](https://www.youtube.com/watch?v=uqofxjeY5fg)
+
+Things you will learn in this video:
+
+1. Add controls to your player
+
+1. Layers, what it is, how to apply them as a Type of Object in the scene and how to add it as dependency in some player action
+
+1. Gravity in RigidBody2D and use it as jump high delimiter
+ 
+    I've found a bug when the player collides to a platform he get stuck. To solve this go to Assets -> Create -> Physical Material 2D and add 0 to friction, then add this material to the platforms.
+
+1. An easy way to script controls to your player.
